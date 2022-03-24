c1ef31488242e4829d28e4b0d32b210e45e74f53
henroikol <87447439+henroikol@users.noreply.github.com>
2022-03-24 18:36:35 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,203 +1,143 @@
-const c = document.getElementById("mainCanvas");
-const ctx = c.getContext('2d');
 
-var playeri = [250,250,0,-10]
+const words = [
+"ma olen burger otse macdonalsist",
+"helenile meeldib eko",
+ "twa arbusa on my busa",
+ "källo on emo",
+ "robot roobert on lahe",
+ "mulle maitsevad kollased maasikad",
+ "juss peeretas persepolis sokratise ees",
+ "su ema sittus persepolis ja mängis amongus"
+]
 
-let obArray = [[0,0,500,100],[400,200,100,500],[200,200,30,500]]; //1 is X 2 is Y 4,5 is size, example: [x,y,w,h]
-
-class object {
-    constructor (canvas,ctx,obi){
-        this.canvas = canvas
-        this.ctx = ctx
-        this.obArray = obi
-    }
-    toCanvasY(y){
-        let ny = this.canvas.height - y
-        return ny
-    }
-    toNormalY (y){
-        let ny = this.canvas.height - y
-        return ny
-    }
-
-    isInside(x,y,w,h){
-        let obA = this.obArray;
-        let isn = false;
-         obA.forEach(function(v){
-            //console.log(v[0],v[1],v[2],v[3])
-            if (x < v[0] + v[2] &&
-                x + w > v[0] &&
-                y < h + v[3] &&
-                h + y > v[1]) {
-                 // collision detected!
-                 console.log("collision")
-                 isn = true
-            } else {
-                //console.log("no collision")
-
-             }
-            
-         });
-         return isn
-         
-    }
-
-    clear(){
-        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    }
-    rectangle(x,y,w,h,c){
-       let ctx = this.ctx
-       let Y = this.toCanvasY(y)
-       ctx.fillStyle = c;
-       ctx.fillRect(x,Y,w,h); 
-    }
-    circle(x,y,s){
-        let nY = this.toCanvasY(y)
-        let ctx = this.ctx
-        
-        ctx.beginPath();
-        ctx.arc(x, nY, s, 0, 2 * Math.PI);
-        ctx.fill();
-        ctx.stroke()
-    }
-    drawArrayObi(){
-        let obA = this.obArray
-        
-        obA.forEach(function(v){
-            
-            functions.rectangle(v[0],v[1],v[2],-v[3],"green")
-        })};
-    playerUpdate(){
-       
-       
-       
-       if (playeri[2]>0){
-        playeri[2] = playeri[2] - 2
-        playeri[0] = playeri[0] - 2
-       } else if (playeri[2] < -1) {
-           playeri[2] = playeri[2] + 2
-           playeri[0] = playeri[0] + 2
-       }
-        
-       if (playeri[3]>0){
-        playeri[3] = playeri[3] - 2
-        playeri[1] = playeri[1] - 2
-       } else if (playeri[3] < -1) {
-        playeri[3] = playeri[3] + 2
-        playeri[1] = playeri[1] + 2
-       } 
-       let isn = functions.isInside(playeri[0],playeri[1],9,9)
-       if (isn ==true){
-        return false
-       }
-       return true
-    }
+const pic = document.getElementById("hangmanpic")
+const charInput = document.getElementById("Character")
+const enterButton = document.getElementById("enterButton")
+const foundwordUi = document.getElementById("foundword");
 
 
 
 
-    
+
+var currentWord = "";
+var foundWord = "";
+var mistakes = 0;
+
+
+
+String.prototype.replaceAt = function(index, replacement) {
+    return this.substr(0, index) + replacement + this.substr(index + replacement.length);
 }
 
-var functions = new object(c,ctx,obArray)
-
-function playermove(v){
-    console.log(v.keyCode)
-    
-    switch (v.keyCode) {
-        case 37: //left
-            if (playeri[2]<25){
-                playeri[2] = playeri[2]+10;
-            
-            }
-            break;
-        case 65: //left 2
-        if (playeri[2]<25){
-                playeri[2] = playeri[2]+10;
-            
-        }
-        break;
-        case 38: //up
-        if (playeri[3]>-25){
-            playeri[3] = playeri[3]-10;
-        }
-            break;
-        case 87: //up 2
-        if (playeri[3]>-25){
-            playeri[3] = playeri[3]-10;
-        }
-                break;
-    
-
-        case 39: //right
-        if (playeri[2]>-25){
-            playeri[2] = playeri[2]-10;
-            }
-            break;
-        case 68: //right 2
-        if (playeri[2]>-25){
-            playeri[2] = playeri[2]-10;
-            }
-            break;
-        case 40: //down,
-            if (playeri[3]<25){
-              playeri[3] = playeri[3]+10;  
-            }
-            
-            break;
-        case 83: //down 2
-        if (playeri[3]<25){
-            playeri[3] = playeri[3]+10;  
-        }
-            
-        break;
-    }
-    
+for(var i=0; i<currentWord.length;i++) {
+    foundWord = foundWord.replaceAt(i,".")   
 }
 
 
-function onMouseMove(m){
-        
-        let X = m.clientX
-        let y = m.clientY
-        //console.log(X,y)
-        
-        let Y = functions.toCanvasY(y)
-        let isN = functions.isInside(X,Y,30,30)
-        //console.log(isN)
-        if (isN == false){
-         
-        //functions.circle(X,Y,10)
-        
-        functions.drawArrayObi();
-        functions.rectangle(X,Y,30,30,"red")
-        }
-        else{
-        
-
-        }
-        
-    }
-document.addEventListener("keydown",playermove)
-c.addEventListener("mousemove",onMouseMove)
 
 
-
-
-function gameloop(){
-    functions.clear()
-    
-    let ish = functions.playerUpdate()
-    if (ish == false){
-        playeri = [c.width/2,c.height/2,0,0]
-    }
-    functions.drawArrayObi();
-    
-    functions.rectangle(c.width/2-10,c.height/2 + 20,30,30,"purple")
-
-    console.log(playeri)
-    functions.rectangle(playeri[0],playeri[1],10,10,"red")
-    
+function chooseWord(){
+    currentWord = words[Math.floor(Math.random() * words.length)];
 }
-setInterval(gameloop,25);
 
+
+
+function isCharInWord(c){
+ return currentWord.includes(c);
+
+}
+
+function ChangePic(){
+    let bef = "hangman pics/";
+    let aft =" mistake.png";
+    let imgsrc = bef + mistakes.toString() + aft;
+    console.log(imgsrc)
+    pic.src = imgsrc;
+}
+
+
+function updateScreenWord(str){
+    let s2 = str.replaceAll('.', '_');
+    let s3 = s2.split('').join(" ");
+    foundwordUi.innerHTML = s3
+}
+
+
+function findAllChar(c){
+    var char = [];
+
+    for(var i=0; i<currentWord.length;i++) {
+
+        if (currentWord[i] === c) {
+
+            char.push(i);
+        }
+        
+    }
+    return char;
+}
+
+
+chooseWord()
+console.log(currentWord)
+
+
+
+
+function Main(c){
+    if (foundWord.length == 0){
+    for(var i=0; i<currentWord.length;i++) {
+            foundWord = foundWord.replaceAt(i,".")   }
+    }
+
+
+
+    updateScreenWord(foundWord)
+
+    console.log(foundWord+"."+currentWord,"mistakes: ", mistakes," fw lenghth: ",foundWord.length)
+
+
+
+    if (currentWord === foundWord){
+        chooseWord()
+        foundWord = ""
+        mistakes = 0
+        
+    }
+
+    if (isCharInWord(c) != true){ mistakes++; ChangePic(); return;}
+    if (mistakes > 6){ mistakes = 0; ChangePic(); foundWord = ""; chooseWord(); return}
+    
+    
+    let fac = findAllChar(c)
+    console.log(fac)
+
+    
+
+    for(var i=0; i<fac.length;i++) {
+        
+        foundWord = foundWord.replaceAt(fac[i],c)   
+    }
+    updateScreenWord(foundWord)
+}
+
+
+
+enterButton.onclick  = function(){
+    let ic = charInput.value;
+    charInput.value = ""
+    console.log(ic);
+    Main(ic)
+
+    
+
+}
+document.addEventListener('keydown', function(event) {
+    if(event.keyCode == 69) {
+        let ic = charInput.value;
+        charInput.value = ""
+        console.log(ic);
+        Main(ic)
+     
+    }
+});
